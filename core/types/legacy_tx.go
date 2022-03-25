bae3588e5f934fa160456977cc3726cd101b280d
Steven Barnett <stevenbarnett1@live.com>
2022-03-24 22:59:11 -0700

    added comma

--- core/types/legacy_tx.go
+++ core/types/legacy_tx.go
@@ -77,7 +77,7 @@ func (tx *LegacyTx) copy() TxData {
 		To:    copyAddressPtr(tx.To),
 		Data:  common.CopyBytes(tx.Data),
 		Gas:   tx.Gas,
-		ChainID: ChainID
+		ChainID: ChainID,
 		// These are initialized below.
 		Value:    new(big.Int),
 		GasPrice: new(big.Int),
